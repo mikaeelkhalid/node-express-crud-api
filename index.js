ead55e790a5d1831394001d49d1db7010e50f682
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2025-12-27 00:00:17 +0100

    Update server configuration and add health check endpoint

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ import bodyParser from "body-parser";
 import usersRoutes from "./routes/users.js";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
 app.use("/people", usersRoutes);
+
 app.get("/", (req, res) => res.send("Welcome to the Users API!"));
+
+app.get('/health', (req, res) => {
+  res.status(200).send('Health check OK');
+});
+
 app.all("*", (req, res) =>
   res.send("You've tried reaching a route that doesn't exist.")
 );
